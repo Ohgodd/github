d09b1b69d956e038184d2fcb9c18c095fbcc9bc6
Ohgodd <buldan2008@bk.ru>
2022-08-03 15:33:09 +0300

    Added time.sleep, also the script works right only if your programme can identify emojis

--- elephant.py
+++ elephant.py
@@ -1,10 +1,40 @@
+import time
+
+
 def elephant_swims(name):
     while True:
-        yes_or_no = input(f'🐘 <--- This is elephant named {name} \n'
+        yes_or_no = input(f'🏠🐘 <--- This is elephant named {name} \n'
                           f'Do you want him to swim? (y/n) ')
-        # не забудь про else
         if yes_or_no == 'y':
+            time.sleep(1)
+            print(f'Please wait the {name} is trying to get to the ocean ....')
+            time.sleep(2)
             print('🏝 🌊 🌊🐘🌊 🌊 🏝')
+            time.sleep(3)
+            print(f'🏝 🌊 🌊🐘🌊 🌊 🏝<--{name} is bored ')
+            time.sleep(0.5)
+            b = input('Should he go home? (y/n) ')
+            if b == 'y':
+                time.sleep(0.5)
+                print('The elephant is running to home currently...')
+                time.sleep(1)
+                print('☀')
+                print('🏠____________🐘💦___🌴🌊🌊🏄‍🌊🌊 ️')
+                print('Please wait...')
+                time.sleep(5)
+            elif b == 'n':
+                print('🏝 🌊 🌊🐘🌊 🌊 🏝')
+                time.sleep(3)
+                print(f'🏝 🌊 🌊🐘🌊 🌊 🏝<--{name} is REALLY bored now and he won\'t listen to you ')
+                time.sleep(1.5)
+                print('The elephant is running to home currently...')
+                time.sleep(1)
+                print('☀')
+                print('🏠____________🐘💦___🌴🌊🌊🏄‍🌊🌊 ️')
+                print('Please wait...')
+                time.sleep(5)
+            else:
+                print('Something went wrong ;(')
         elif yes_or_no == 'n':
             quit1 = input('So you want to quit? (y/n) ')
             if quit1 == 'y':
