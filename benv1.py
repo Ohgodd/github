6d225bbcbc44b41737b6c6645ef44545af812ea4
Ohgodd <buldan2008@bk.ru>
2022-08-03 22:46:59 +0300

    ...

--- benv1.py
+++ benv1.py
@@ -1,11 +1,13 @@
 # pocket ben v1
 import random
-
-gay = False
+import pygame
+pygame.mixer.init()
 command = ''
 
 
 def ben_generator():
+    pygame.mixer.music.load('Ben.mp3')
+    pygame.mixer.music.play()
     print('You are currently talk to Ben')
     while True:
         command2 = input('Input your question: ')
@@ -14,6 +16,18 @@ def ben_generator():
             print('The Ben is coming for you ')
             break
         ben_answer = random.choice(list2)
+        if ben_answer == 'Yes':
+            pygame.mixer.music.load('Yes.mp3')
+            pygame.mixer.music.play()
+        elif ben_answer == 'No':
+            pygame.mixer.music.load('No.mp3')
+            pygame.mixer.music.play()
+        elif ben_answer == 'Ugh...':
+            pygame.mixer.music.load('Ugh.mp3')
+            pygame.mixer.music.play()
+        elif ben_answer == 'HOHOHO':
+            pygame.mixer.music.load('Hohoho.mp3')
+            pygame.mixer.music.play()
         print(ben_answer)
 
 
--- elephant.py
+++ elephant.py
@@ -1,34 +1,60 @@
 import time
+import pygame
+
+pygame.mixer.init()
 
 
 def elephant_swims(name):
+    elephant_trumpenting = pygame.mixer.Sound('ElephantTrumpeting.mp3')
+    footsteps_on_grass = pygame.mixer.Sound('Footsteps(grass).mp3')
+    ocean = pygame.mixer.Sound('ocean(3sec).mp3')
+    footsteps = pygame.mixer.Sound('Footsteps2.mp3')
+    ocean2 = pygame.mixer.Sound('ocean(1.5sec).mp3')
     while True:
+        elephant_trumpenting.play()
+        elephant_trumpenting.set_volume(0.3)
         yes_or_no = input(f'🏠🐘 <--- This is elephant named {name} \n'
                           f'Do you want him to swim? (y/n) ')
         if yes_or_no == 'y':
             time.sleep(1)
             print(f'Please wait the {name} is trying to get to the ocean ....')
+            footsteps_on_grass.play()
+            footsteps_on_grass.set_volume(0.5)
             time.sleep(2)
             print('🏝 🌊 🌊🐘🌊 🌊 🏝')
+            elephant_trumpenting.play()
+            ocean.play()
+            ocean.set_volume(0.5)
             time.sleep(3)
             print(f'🏝 🌊 🌊🐘🌊 🌊 🏝<--{name} is bored ')
-            time.sleep(0.5)
+            ocean.play()
+            time.sleep(2)
             b = input('Should he go home? (y/n) ')
             if b == 'y':
                 time.sleep(0.5)
                 print('The elephant is running to home currently...')
+                elephant_trumpenting.play()
                 time.sleep(1)
+                footsteps.play()
                 print('☀')
                 print('🏠____________🐘💦___🌴🌊🌊🏄‍🌊🌊 ️')
                 print('Please wait...')
                 time.sleep(5)
             elif b == 'n':
+                elephant_trumpenting.play()
                 print('🏝 🌊 🌊🐘🌊 🌊 🏝')
+                ocean.play()
+                ocean.set_volume(0.5)
                 time.sleep(3)
                 print(f'🏝 🌊 🌊🐘🌊 🌊 🏝<--{name} is REALLY bored now and he won\'t listen to you ')
-                time.sleep(1.5)
+                ocean2.play()
+                ocean2.set_volume(0.5)
+                elephant_trumpenting.play()
+                time.sleep(2)
                 print('The elephant is running to home currently...')
+                elephant_trumpenting.play()
                 time.sleep(1)
+                footsteps.play()
                 print('☀')
                 print('🏠____________🐘💦___🌴🌊🌊🏄‍🌊🌊 ️')
                 print('Please wait...')
